61ebf8059c59ee28014537ede8f6b5b4bc4968b9
Omer Galiko <omergaliko22@gmail.com>
2021-02-08 14:00:15 +0200

    main.tf

---
+++ main.tf
@@ -0,0 +1,46 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+        }     
+    }   
+}
+provider "aws" { 
+    region = var.region 
+    access_key = "AKIAJIKJFCHQA67VX5TQ" 
+    secret_key = "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" 
+
+} 
+module "network" {
+    source = "./module_vpc_subnet"
+    vpc_cidr_block = "10.0.0.0/16"
+    public_subnet  = "10.0.0.0/24"
+    private_subnet = "10.0.10.0/24"
+}
+
+module "security_group" {
+    source = "./module_security_group"
+    vpc_id = module.network.aws_vpc 
+}
+
+module "instance_public" {
+    source = "./module_instance"
+    availability_zone = "us-east-2a"
+    ami = "ami-0a91cd140a1fc148a"
+    key_name = "key_pem"
+    instance_type = "t2.micro"
+    subnet_id = module.network.aws_subnet_public
+    security_group = module.security_group.public_security_group
+    Tag_name = "Nginx"
+}
+
+module "instance_private" {
+    source = "./module_instance"
+    availability_zone = "us-east-2a"
+    ami = "ami-0a91cd140a1fc148a"
+    key_name = "key_pem"
+    instance_type = "t2.micro"
+    subnet_id = module.network.aws_subnet_private
+    security_group = module.security_group.private_security_group
+    Tag_name = "MySQL"
+}
